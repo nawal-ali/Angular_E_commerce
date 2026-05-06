4afc54392bb8382af5d14e6f75242ba965ca926e
nawal-ali <nmaaa3899@gmail.com>
2026-05-06 18:47:18 +0300

    created project structure

---
+++ src/app/pages/single-product/single-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-product',
+  imports: [],
+  templateUrl: './single-product.component.html',
+  styleUrl: './single-product.component.css'
+})
+export class SingleProductComponent {
+
+}

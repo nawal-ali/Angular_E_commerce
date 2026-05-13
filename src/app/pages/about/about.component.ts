7c4e699f2ebde5cc2b658feb539ae141eca8e15e
Afaf Nossier <afafnossier@gmail.com>
2026-05-13 06:49:35 +0300

    Add About Page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import {  OnInit } from '@angular/core';
+import * as AOS from 'aos' ; 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      once: true,      
+      offset: 100    
+    });
+  }
 }
+ 
